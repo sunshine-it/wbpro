e15bcb8e1a397eac3d8fb74acb7f837088385e93
Allen <pazmnew@163.com>
2021-10-05 06:34:35 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+<div class="jumbotron">
+    <h1>Hello Laravel8</h1>
+    <p class="lead">
+    你现在所看到的是 <a href="https://learnku.com/docs/laravel/8.5">Laravel8.x</a> 的中文文档主页。
+    </p>
+    <p>
+    一切，将从这里开始。
+    </p>
+    <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+</div>
 @stop
