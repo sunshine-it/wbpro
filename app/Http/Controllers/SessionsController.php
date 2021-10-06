46febf39c9e280059d6ceeb3b602ffeb869f4ed3
Allen <pazmnew@163.com>
2021-10-06 09:54:48 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    //显示登录页面
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    //创建新会话（登录）
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // 登录成功后的相关操作
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        }
+        else{
+            // 登录失败后的相关操作
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+
+        return;
+    }
+
+}
