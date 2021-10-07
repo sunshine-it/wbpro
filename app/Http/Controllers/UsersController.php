fc1fe76ee2c719bcc4ac5526655e802106594cb0
Allen <pazmnew@163.com>
2021-10-07 07:53:50 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -88,4 +88,14 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
         return redirect()->route('users.show', $user->id);
     }
+
+    // 删除用户
+    public function destroy(User $user)
+    {
+        // 删除策略
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
