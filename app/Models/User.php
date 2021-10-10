46d7ffc1dc2fba05e0067dc30eed886222c84085
Allen <pazmnew@163.com>
2021-10-10 10:30:32 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,4 +76,41 @@ class User extends Authenticatable
         return $this->statuses()
                 ->orderBy('created_at', 'desc');
     }
+
+    //以粉丝为例，一个用户能够拥有多个粉丝(粉丝关系)
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 用户关注
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 定义关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 定义取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
 }
